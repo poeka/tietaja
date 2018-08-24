288898fec8912b79a7e7a8341f7b090517f6e65a
Mikko Pihlanen <pihlanem@student.tut.fi>
2018-08-25 00:59:02 +0300

    Load bootstrap from cdn. Registration styling.

--- __init__.py
+++ __init__.py
@@ -3,10 +3,11 @@ import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    
+
     Bootstrap(app)
 
     app.config.from_mapping(
